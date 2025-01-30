e8529e2e60659efb4c641ed56751abab6b14cb33
ZingerEngineer <ramy.yasser21102@gmail.com>
2025-01-30 16:56:11 +0200

    Created public router.

---
+++ apps/server/routers/publicRouter.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express'
+import authRouter from './authRouter'
+const publicRouter = express.Router()
+
+publicRouter.get('/', (_, res) => {
+  res.json({ message: 'Hello World' })
+})
+
+publicRouter.use('/auth/v1', authRouter)
+
+export default publicRouter
+
