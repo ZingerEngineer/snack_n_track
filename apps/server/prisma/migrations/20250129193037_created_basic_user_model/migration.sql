c1adc9b8e8bd4cf135a1ec861bd300ed4ba53484
ZingerEngineer <ramy.yasser21102@gmail.com>
2025-01-30 14:31:14 +0200

    Removed firebase configs and dependencies and removed uploads from git cache

---
+++ apps/server/prisma/migrations/20250129193037_created_basic_user_model/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "age" INTEGER,
+ADD COLUMN     "dietary_preferences" TEXT,
+ADD COLUMN     "gender" TEXT,
+ADD COLUMN     "goals" TEXT,
+ADD COLUMN     "medical_conditions" TEXT,
+ADD COLUMN     "password" TEXT;
