3d6fa2378b8a78827e437974491afab4d9c33dcb
ZingerEngineer <ramy.yasser21102@gmail.com>
2025-02-12 02:08:26 +0200

    Removed unnecessray logic and imports from scanview  and created new ui.

--- apps/mobile/src/views/ScanView.vue
+++ apps/mobile/src/views/ScanView.vue
@@ -1,70 +1,42 @@
 <script setup lang="ts">
-import { cameraOutline, trash } from 'ionicons/icons'
-import { IonPage, IonContent, IonIcon, IonButton } from '@ionic/vue'
-import { ref } from 'vue'
-import pickPicture from '../apis/mobile/pickPicture'
-import { INutritionData, IEstimatedNutritionData } from '../types/global.types'
-
-const imagePath = ref<string | null>(null)
-const nutritionData = ref<null | INutritionData | IEstimatedNutritionData>(null)
-const pickPhotoHandler = async () => {
-  const imgBlobUrl = await pickPicture()
-  if (!imgBlobUrl) return
-  imagePath.value = imgBlobUrl
-}
-
-const resetPhoto = () => {
-  if (imagePath.value) URL.revokeObjectURL(imagePath.value)
-  imagePath.value = null
-}
-
-const anaylsePhotoHandler = async () => {
-  if (!imagePath.value) return
-  const imgBlob = await fetch(imagePath.value).then((res) => res.blob())
-  const formData = new FormData()
-  formData.append('file', imgBlob)
-  const response = await fetch('http://localhost:3000/v1/private/scan', {
-    method: 'POST',
-    body: formData,
-    credentials: 'include',
-  })
-
-  const data = await response.json()
-  nutritionData.value = data.response
-}
+import { IonPage, IonContent, IonButton } from '@ionic/vue'
+import { useScanStore } from '../stores/scan.store'
+import { faCameraAlt, faImages } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+const { pickPhotoHandler, resetPhoto, anaylsePhotoHandler, imagePath, nutritionData } =
+  useScanStore()
 </script>
 
 <template>
   <ion-page>
     <ion-content>
-      <div class="content-wrapper flex flex-col items-center justify-center">
-        <div class="p-4 flex flex-col justify-center items-center gap-4">
-          <div class="flex flex-col justify-center items-center">
-            <div class="relative">
-              <button
-                v-if="imagePath"
-                class="absolute -bottom-4 -left-4 bg-red-600 rounded-full p-2 hover:bg-red-400 duration-150"
-                @click="resetPhoto"
-              >
-                <ion-icon size="large" :icon="trash" class="text-4xl text-white" />
-              </button>
-              <div
-                @click="pickPhotoHandler"
-                class="flex justify-center items-center aspect-square overflow-hidden w-64 border-2 border-dashed border-gray-400 rounded-lg"
-              >
-                <img
-                  v-if="imagePath"
-                  :src="imagePath"
-                  alt="Picked Image"
-                  class="object-cover w-full h-full"
-                />
-                <div
-                  v-else
-                  class="flex flex-col justify-center items-center w-full h-full bg-gray-200"
-                >
-                  <ion-icon :icon="cameraOutline" class="text-4xl text-gray-400" />
-                  <p class="text-gray-400 font-bold">Tap to add an image.</p>
-                </div>
+      <div class="content-wrapper h-full flex flex-col items-center justify-center">
+        <div class="w-full h-full flex flex-col justify-center items-center gap-4">
+          <div class="flex flex-col w-full h-full justify-center items-center">
+            <div
+              @click="pickPhotoHandler"
+              class="bg-primary-shade relative w-full h-full flex justify-center items-center z-[1]"
+            >
+              <FontAwesomeIcon
+                :icon="faCameraAlt"
+                class="text-[15rem] absolute z-[1] text-white/30 left-1 rotate-12"
+              ></FontAwesomeIcon>
+              <div class="flex flex-col items-center justify-center gap-2 z-[2]">
+                <FontAwesomeIcon :icon="faCameraAlt" class="text-5xl text-white"></FontAwesomeIcon>
+                <p class="text-white font-bold text-sm">Take an instant photo.</p>
+              </div>
+            </div>
+            <div
+              @click="pickPhotoHandler"
+              class="bg-tertiary relative w-full h-full flex justify-center items-center z[1]"
+            >
+              <FontAwesomeIcon
+                :icon="faImages"
+                class="text-[15rem] absolute z-[1] text-white/30 right-1 -rotate-12"
+              ></FontAwesomeIcon>
+              <div class="flex flex-col items-center justify-center gap-2 z-[2]">
+                <FontAwesomeIcon :icon="faImages" class="text-5xl text-white"></FontAwesomeIcon>
+                <p class="text-white font-bold text-sm">Grab a photo from the gallery.</p>
               </div>
             </div>
           </div>
