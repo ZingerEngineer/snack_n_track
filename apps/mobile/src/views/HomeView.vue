e6e6b5253576de7add8c8d7851e748fa779177c1
ZingerEngineer <ramy.yasser21102@gmail.com>
2025-02-12 02:06:53 +0200

    Removed unnecessary imports and logic  and created home view. awaiting new logic for user data (calories and name,etc).

--- apps/mobile/src/views/HomeView.vue
+++ apps/mobile/src/views/HomeView.vue
@@ -1,64 +1,169 @@
 <script setup lang="ts">
-import { IonPage, IonContent, IonIcon } from '@ionic/vue'
+import { IonPage, IonContent, IonButton } from '@ionic/vue'
 import { useAuthStore } from '../stores/auth.store'
-import { logOutOutline } from 'ionicons/icons'
-import ToastService from '../services/ToastService'
 import { useRouter } from 'vue-router'
 
-const router = useRouter()
-const { logout, user } = useAuthStore()
+import {
+  faBowlRice,
+  faPlateWheat,
+  faUtensils,
+  faMedal,
+  faShoePrints,
+  faCog,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-const handleLogout = async () => {
-  try {
-    await logout()
-    ToastService.success('Logged out successfully')
-    router.push('/login')
-  } catch (error) {
-    console.error('Logout failed:', error)
-    ToastService.error('Logout failed')
-  }
+const router = useRouter()
+const { user } = useAuthStore()
+
+const handleScanRouting = () => {
+  router.push('/dashboard/scan')
 }
 </script>
 
 <template>
   <ion-page>
     <ion-content>
-      <div class="w-full h-full">
-        <ion-card class="p-2">
-          <ion-card-header>
-            <ion-card-title>Welcome home.</ion-card-title>
-          </ion-card-header>
+      <div class="p-6 w-full h-full flex items-center flex-col gap-6">
+        <ion-card
+          class="p-2 w-full border-2 to-primary from-primary/50 bg-gradient-to-l border-primary rounded-2xl shadow-lg"
+        >
           <ion-card-content>
             <div class="w-full h-full flex flex-row justify-between items-center">
-              <div>
-                <ion-icon
-                  @click="handleLogout"
-                  class="text-emerald-400 rounded-xl p-2 text-[3rem]"
-                  :icon="logOutOutline"
-                />
-              </div>
-              <div
-                class="flex flex-row items-center justify-center bg-emerald-400 px-5 py-2 rounded-full text-white border-emerald-300 border-2 gap-2"
-              >
-                <p class="">
-                  {{ user?.email }}
-                </p>
+              <div class="flex flex-row items-center justify-center">
                 <img
-                  class="w-12 bg-cover bg-center rounded-full"
+                  class="w-16 bg-cover bg-center rounded-full"
                   src="https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3383.jpg?semt=ais_hybrid"
-                  alt="Random"
+                  alt="user avatar"
                 />
+                <div class="text-white flex flex-col ml-2">
+                  <h1>Welcome, {{ user.name ? user.name : 'Guest' }}</h1>
+                  <p class="text-sm">{{ user?.email }}</p>
+                </div>
               </div>
+              <FontAwesomeIcon :icon="faCog" class="text-white text-4xl" />
             </div>
           </ion-card-content>
         </ion-card>
+        <ion-card
+          class="p-2 border-2 to-light from-medium bg-gradient-to-l border-medium-shade rounded-2xl shadow-lg w-full"
+        >
+          <ion-card-header>
+            <ion-card-title> Meals </ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            <div class="w-full h-full flex flex-col justify-between items-center gap-2">
+              <div class="w-full h-full flex flex-row justify-between items-center">
+                <div class="flex flex-col items-center justify-center">
+                  <h2>0 kCals</h2>
+                  <div class="flex flex-row items-center justify-center gap-2">
+                    <p class="text-sm">Breakfast</p>
+                    <FontAwesomeIcon :icon="faPlateWheat" />
+                  </div>
+                </div>
+                <div class="flex flex-col items-center justify-center">
+                  <h2>0 kCals</h2>
+                  <div class="flex flex-row items-center justify-center gap-2">
+                    <p class="text-sm">Lunch</p>
+                    <FontAwesomeIcon :icon="faUtensils" />
+                  </div>
+                </div>
+                <div class="flex flex-col items-center justify-center">
+                  <h2>0 kCals</h2>
+                  <div class="flex flex-row items-center justify-center gap-2">
+                    <p class="text-sm">Dinner</p>
+                    <FontAwesomeIcon :icon="faBowlRice" />
+                  </div>
+                </div>
+              </div>
+              <ion-button @click="handleScanRouting" class="w-full text-white"
+                >Scan meal</ion-button
+              >
+            </div>
+          </ion-card-content>
+        </ion-card>
+        <div class="w-full flex flex-row justify-between items-center gap-6">
+          <ion-card
+            class="p-2 border-2 to-light from-medium bg-gradient-to-l border-medium-shade rounded-2xl shadow-lg h-full w-3/5"
+          >
+            <ion-card-header>
+              <ion-card-title> Total calories <FontAwesomeIcon :icon="faMedal" /></ion-card-title>
+            </ion-card-header>
+            <ion-card-content class="flex items-center justify-center">
+              <svg class="w-36 h-36 mb-2" viewBox="0 0 64 64">
+                <circle
+                  class="text-medium-shade"
+                  stroke-width="4"
+                  stroke-dasharray="188"
+                  stroke-dashoffset="0"
+                  stroke-linecap="round"
+                  stroke="currentColor"
+                  fill="transparent"
+                  r="30"
+                  cx="32"
+                  cy="32"
+                />
+                <circle
+                  class="calories-complete-circle text-primary"
+                  stroke-width="4"
+                  stroke-dasharray="188"
+                  :stroke-dashoffset="188 - (188 * 30) / 100"
+                  transform="rotate(-90 32 32)"
+                  stroke="currentColor"
+                  fill="transparent"
+                  r="30"
+                  cx="32"
+                  cy="32"
+                />
+                <text
+                  class="text-xl calories-complete-percent"
+                  x="50%"
+                  y="50%"
+                  dominant-baseline="middle"
+                  text-anchor="middle"
+                >
+                  100
+                </text>
+              </svg>
+            </ion-card-content>
+          </ion-card>
+          <ion-card
+            class="flex flex-col p-2 border-2 to-light from-medium bg-gradient-to-l border-medium-shade rounded-2xl shadow-lg h-full w-2/5"
+          >
+            <ion-card-header>
+              <ion-card-title> Steps </ion-card-title>
+            </ion-card-header>
+            <ion-card-content class="flex flex-grow">
+              <div class="w-full h-full flex flex-col justify-center items-center gap-4">
+                <div class="flex flex-col items-center justify-center">
+                  <h1 class="text-4xl">0</h1>
+                  <div class="flex flex-row items-center justify-center gap-2">
+                    <p class="text-sm">Today</p>
+                    <FontAwesomeIcon :icon="faShoePrints" />
+                  </div>
+                </div>
+                <div class="flex flex-col items-center justify-center">
+                  <h1 class="text-4xl">0</h1>
+                  <div class="flex flex-row items-center justify-center gap-2">
+                    <p class="text-sm">Average</p>
+                    <FontAwesomeIcon :icon="faMedal" />
+                  </div>
+                </div>
+              </div>
+            </ion-card-content>
+          </ion-card>
+        </div>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <style scoped>
-ion-page {
-  color: black;
+.calories-complete-circle {
+  transition: all 1s;
+}
+.calories-complete-percent {
+  transition: all 1s;
+  fill: var(--ion-color-medium-shade);
 }
 </style>
