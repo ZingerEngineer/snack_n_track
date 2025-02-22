c301592bd8202960a06a00849cf428b4f987874e
ZingerEngineer <ramy.yasser21102@gmail.com>
2025-02-22 05:16:45 +0200

    Created and optimized deploy script.

--- apps/server/deploy.sh
+++ apps/server/deploy.sh
@@ -1,12 +1,14 @@
 
 echo "Pulling changes..." &&
+cd ~/snack_n_track &&
 git stash &&
 git pull origin main &&
 echo "Installing dependencies..." &&
 yarn && 
+cd apps/server &&
 echo "Applying DB changes..." &&
-npx prisma generate --schema=./src/prisma/schema.prisma &&
-npx prisma migrate deploy --schema=./src/prisma/schema.prisma  &&
+npx prisma generate --schema="./src/prisma/schema.prisma" &&
+npx prisma migrate deploy --schema="./src/prisma/schema.prisma"  &&
 echo "Building project artifcats..." &&
 yarn build &&
 cp .env ./dist/.env &&
