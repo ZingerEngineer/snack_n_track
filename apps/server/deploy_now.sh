19db3e1ba4eb22417715443b4f7deed46cef6f18
ZingerEngineer <ramy.yasser21102@gmail.com>
2025-02-23 21:41:23 +0200

    Changed deployment ip address.

--- apps/server/deploy_now.sh
+++ apps/server/deploy_now.sh
@@ -1,7 +1,7 @@
 echo "Connecting to EC2 instance..." &&
 
 ssh root@MachineB 'bash -s' < local_script.sh
-ssh -i "~/ssh/snack_n_track_web_server_key.pem" ubuntu@13.48.138.37 'bash -s' < deploy.sh
+ssh -i "~/ssh/snack_n_track_web_server_key.pem" ubuntu@13.61.186.48 'bash -s' < deploy.sh
 # cd ~/snack-n-track &&
 # echo "Pulling changes..." &&
 # git stash &&
