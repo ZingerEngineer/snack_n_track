7717553320d60c807da567e6a0f952cc3ed7d60f
ZingerEngineer <ramy.yasser21102@gmail.com>
2025-02-24 00:40:13 +0200

    headless true

--- apps/server/src/services/caloriesCalculatorGpt.serviceRevamped.ts
+++ apps/server/src/services/caloriesCalculatorGpt.serviceRevamped.ts
@@ -43,7 +43,7 @@ async function scrappingCalculator(
   try {
     console.log('[scrappingCalculator] Launching browser')
     browser = await puppeteer.launch({
-      headless: 'shell',
+      headless: true,
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
