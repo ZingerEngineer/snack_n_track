973b5a2d6c8f563ea69505f57070e20bfda31be7
ZingerEngineer <ramy.yasser21102@gmail.com>
2025-02-24 03:39:06 +0200

    Trying headless false

--- apps/server/src/services/caloriesCalculatorGpt.serviceRevamped.ts
+++ apps/server/src/services/caloriesCalculatorGpt.serviceRevamped.ts
@@ -53,7 +53,7 @@ async function scrappingCalculator(
   try {
     console.log('[scrappingCalculator] Launching browser')
     browser = await puppeteerExtra.launch({
-      headless: true,
+      headless: false,
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
